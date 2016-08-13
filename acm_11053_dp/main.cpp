a7a489a98433deb83872afa15ae8feb1009ca78d
yohan <xkth342@gmail.com>
2016-08-13 13:55:02 +0900

    <yohan> acm

---
+++ acm_11053_dp/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int n, t;
+vector<int> arr;
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &t);
+
+        vector<int>::iterator a = lower_bound(arr.begin(), arr.end(), t);
+        if (a == arr.end()) arr.push_back(t);
+        else *a = t;
+    }
+
+
+    printf("%d", arr.size());
+}
